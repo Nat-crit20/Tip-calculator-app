0bbc3da96565d6be9ae7a5d855eff2bf168e981a
Nathan Greuel <greuelnathan74@gmail.com>
2022-09-18 16:05:42 -0500

    working on text inputs

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const form = document.querySelector("form");
 
-$("#other-field").focus(function () {
-  $("#other").prop("checked", true);
-});
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const data = new FormData(form);
-  for (const pair of data.entries()) {
-    console.log(`${pair[0]}, ${pair[1]}`);
-  }
-});
