ac731c2ef2c5d569e6d61883ab67f49d97b9926d
Nathan Greuel <greuelnathan74@gmail.com>
2022-09-20 15:49:35 -0500

    Finished basic javascirpt logic

--- script.js
+++ script.js
@@ -4,10 +4,40 @@ const payPerPerson = document.querySelector(".final-per-person");
 const billInput = document.getElementById("bill");
 const customTip = document.getElementById("custom-input");
 const numPeople = document.getElementById("num-people");
+const tipBtn = document.querySelectorAll(".tip-button");
+
+///
+
+let tip = 25;
+let bill = 0;
+let people = 1;
 
 //Functions
 
+const calcTip = function (bill, tipPercent, totalPeople) {
+  let tipAmount = bill * (tipPercent / 100);
+  let results = (bill + tipAmount) / totalPeople;
+  console.log(results);
+  return results;
+};
+
 //EvetntListiners
+customTip.addEventListener("input", function (event) {
+  event.preventDefault();
+  let input = Number(event.target.value);
+  if (isNaN(input)) {
+    throw new Error("This is not a number");
+  }
+  tip = input;
+  calcTip(bill, tip, people);
+});
+
+tipBtn.forEach((btn) => {
+  btn.addEventListener("click", function (event) {
+    tip = event.target.innerText.slice(0, -1);
+    calcTip(bill, tip, people);
+  });
+});
 
 billInput.addEventListener("input", function (event) {
   event.preventDefault();
@@ -15,7 +45,8 @@ billInput.addEventListener("input", function (event) {
   if (isNaN(input)) {
     throw new Error("This is not a number");
   }
-  console.log(input);
+  bill = input;
+  calcTip(bill, tip, people);
 });
 
 numPeople.addEventListener("input", function (event) {
@@ -30,5 +61,6 @@ numPeople.addEventListener("input", function (event) {
   if (input < 0) {
     throw new Error("Must be greater than zero");
   }
-  console.log(input);
+  people = input;
+  calcTip(bill, tip, people);
 });
