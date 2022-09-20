4922c2ce7da38d8b9d3bb1bcc6a25b718b3ebf5c
Nathan Greuel <greuelnathan74@gmail.com>
2022-09-20 14:22:13 -0500

    started javascript

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const reset = document.querySelector(".reset-btn");
+const totalBill = document.querySelector(".final-total");
+const payPerPerson = document.querySelector(".final-per-person");
+const billInput = document.getElementById("bill");
+const customTip = document.getElementById("custom-input");
+const numPeople = document.getElementById("num-people");
 
+//Functions
+
+//EvetntListiners
+
+billInput.addEventListener("input", function (event) {
+  event.preventDefault();
+  let input = Number(event.target.value);
+  if (isNaN(input)) {
+    throw new Error("This is not a number");
+  }
+  console.log(input);
+});
+
+numPeople.addEventListener("input", function (event) {
+  event.preventDefault();
+  let input = Number(event.target.value);
+  if (isNaN(input)) {
+    throw new Error("This is not a number");
+  }
+  if (input === 0) {
+    throw new Error("Can't be zero");
+  }
+  if (input < 0) {
+    throw new Error("Must be greater than zero");
+  }
+  console.log(input);
+});
